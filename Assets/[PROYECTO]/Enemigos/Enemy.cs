f70bd015bdca653d7cd227d30f3d0a3f35b74d3f
Arnau <clavileno3000@gmail.com>
2025-01-13 19:41:05 +0100

    Enemigo 2.0 y Cambios en GUI
    Ahora la GUI no se da sino que se busca por nombre en el Enemy.cs para los cambios de Vida y Puntuacion.
    
    El enemigo ahora busca por Tag al Paciente para saber en qué dirección mirar.

--- Assets/[PROYECTO]/Enemigos/Enemy.cs
+++ Assets/[PROYECTO]/Enemigos/Enemy.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public TextMeshProUGUI scoreText;
+    private TextMeshProUGUI scoreText;
+    private TextMeshProUGUI hpText;
     public int ScoreSuma = 10;
+    public int HPresta = 10;
 
     public Vector3 movement;
     private GameObject enemy;
     public GameObject paciente;
 
-    public float speed = 0.5f;
+    public float speed = 0.025f;
 
     private void Start()
     {
         enemy = this.gameObject;
+        scoreText = GameObject.Find("puntuacion/labelPuntuacion/textPuntuacion").GetComponent<TextMeshProUGUI>();
+        hpText = GameObject.Find("vida/labelHp/textHp").GetComponent<TextMeshProUGUI>();
+
+        mirarPaciente();
+    }
+
+    private void mirarPaciente()
+    {
+        GameObject paciente = GameObject.FindWithTag("paciente");
+        if (paciente != null)
+        {
+            // Haz que el enemigo mire hacia el paciente
+            enemy.transform.LookAt(paciente.transform);
+        }
+        else
+        {
+            Debug.LogError("El objeto paciente no estÃ¡ asignado.");
+        }
     }
 
     private void Update()
     {
-
+        //Moverse
         enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, paciente.transform.position, speed);
     }
     private void OnCollisionEnter(Collision collision)
@@ -42,7 +62,28 @@ public class Enemy : MonoBehaviour
                 }
                 else 
                 {
-                    Debug.LogError("El texto de Score no es un número válido");
+                    Debug.LogError("El texto de Score no es un numero valido");
+                }
+            }
+            //Destruyete
+            Destroy(gameObject);
+        }
+
+        // Si el enemigo toca al paciente
+        if(collision.gameObject.CompareTag("paciente"))
+        {
+            // Actualiza el texto del HP
+            if (hpText != null)
+            {
+                int currentHP;
+                if (int.TryParse(hpText.text.Replace("HP: ", ""), out currentHP))
+                {
+                    currentHP -= HPresta;
+                    hpText.text = "" + currentHP;
+                }
+                else
+                {
+                    Debug.LogError("El texto de HP no es un numero valido.");
                 }
             }
             //Destruyete
